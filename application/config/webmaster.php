5deaffa8c875fb1a208d407cb9acd12289418b39
matadata11 <94419175+matadata11@users.noreply.github.com>
2022-07-25 11:04:12 +0700

    awal

---
+++ application/config/webmaster.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['version']      = '1.1 Beta';
+$config['Aauthor']      = 'Not Found dot ID';
+$config['sistem']       = 'Birawa.id';
+$config['email']        = 'matadata.dev2021@gmail.com';
+$config['dev']          = 'Bidang Pembinaan SMK Aceh';
