e99b7bf4a65df570aaff95bffbe6fa5b105466b8
UDEEEN <imannuddin2096@gmail.com>
2022-07-29 17:20:43 +0700

    new

--- public/js/nautica.js
+++ public/js/nautica.js
@@ -1,15 +1,17 @@
-$("#livestock1,#livestock2,#livestock3,#livestock4,#livestock5").keyup(function() {
-    var val1 = $('#livestock1').val(); 
-	var val2 = $('#livestock2').val(); 
-	var val3 = $('#livestock3').val(); 
-	var val4 = $('#livestock4').val(); 
-	var val5 = $('#livestock5').val(); 
+$("#nautica1,#nautica2,#nautica3,#nautica4,#nautica5,#nautica6,#nautica7").keyup(function() {
+    var val1 = $('#nautica1').val(); 
+	var val2 = $('#nautica2').val(); 
+	var val3 = $('#nautica3').val(); 
+	var val4 = $('#nautica4').val(); 
+	var val5 = $('#nautica5').val(); 
+	var val5 = $('#nautica6').val(); 
+	var val5 = $('#nautica7').val(); 
 
 
-	var kali = Number(val1) + Number(val2) + Number(val3) + Number(val4) + Number(val5)  ;
+	var kali = Number(val1) + Number(val2) + Number(val3) + Number(val4) + Number(val5) + Number(val6) + Number(val7)  ;
 
-	if ( val1 != "" && val2 != "" && val3 != "" && val4 != "" && val5 != ""  ) 
+	if ( val1 != "" && val2 != "" && val3 != "" && val4 != "" && val5 != "" && val6 != "" && val7 != ""  ) 
     {
-    $('#livestocka').val(kali);
+    $('#nauticaa').val(kali);
 	}
 })
