399f5e53debe3a77f4f4f7f74ae3edf5dca3af0d
matadata11 <94419175+matadata11@users.noreply.github.com>
2022-07-29 18:40:32 +0700

    dwi

--- application/views/backend/main.php
+++ application/views/backend/main.php
@@ -200,7 +200,6 @@
     <script src="<?=__js('refrigeration.js')?>"></script> 
     <script src="<?=__js('restaurant.js')?>"></script> 
     <script src="<?=__js('design.js')?>"></script> 
-    <script src="<?=__js('wall.js')?>"></script> 
     
 	<script>
     $(document).ready(function(){
