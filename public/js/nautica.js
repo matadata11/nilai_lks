f49d954f6406f7cce3556e85725049bb5cad7f94
UDEEEN <imannuddin2096@gmail.com>
2022-07-29 17:26:11 +0700

    new

--- public/js/nautica.js
+++ public/js/nautica.js
@@ -4,8 +4,8 @@ $("#nautica1,#nautica2,#nautica3,#nautica4,#nautica5,#nautica6,#nautica7").keyup
 	var val3 = $('#nautica3').val(); 
 	var val4 = $('#nautica4').val(); 
 	var val5 = $('#nautica5').val(); 
-	var val5 = $('#nautica6').val(); 
-	var val5 = $('#nautica7').val(); 
+	var val6 = $('#nautica6').val(); 
+	var val7 = $('#nautica7').val(); 
 
 
 	var kali = Number(val1) + Number(val2) + Number(val3) + Number(val4) + Number(val5) + Number(val6) + Number(val7)  ;
